0f0dfc406d083c0864d20da4b7224cf9f5e0d3e2
Christopher Martin <tmajibon@gmail.com>
2014-02-28 18:25:43 -0600

    CLI Update - Added Dossier List by DDI/Host, Get All, and Get Latest. Cleaned up collecting DDI and host from user

--- waldo-cli.py
+++ waldo-cli.py
@@ -3,30 +3,7 @@ __version__ = 0.1
 
 import waldo
 
-
-def create_dossier():
-
-	print "---------------------------------"
-	print "Create Dossier"
-	print "---------------------------------"
-	print
-
-	valid_input = False
-
-	while not valid_input:
-		ddi = raw_input("Please enter DDI: ")
-
-		ddi = ddi.strip(' ')
-
-		if len(ddi) > 0 and ddi.isdigit():
-			ddi = int(ddi)
-			valid_input = True
-		elif len(ddi) == 0:
-			print "No DDI entered, cancelling."
-			return
-		else:
-			print "Invalid DDI Input."
-
+def get_host():
 	valid_input = False
 
 	while not valid_input:
@@ -37,8 +14,34 @@ def create_dossier():
 		if len(host) > 0:
 			valid_input = True
 		else:
-			print "No hosted entered or invalid host. Cancelling."
-			return
+			print "Invalid host input."
+	return host
+
+def get_ddi():
+	valid_input = False
+
+	while not valid_input:
+		ddi = raw_input("Please enter DDI: ")
+
+		ddi = ddi.strip(' ')
+
+		if len(ddi) > 0 and ddi.isdigit():
+			ddi = int(ddi)
+			valid_input = True
+		else:
+			print "Invalid DDI Input."
+	return ddi
+
+def create_dossier():
+
+	print "---------------------------------"
+	print "Create Dossier"
+	print "---------------------------------"
+	print
+
+	ddi = get_ddi()
+
+	host = get_host()
 
 	status, result = waldo.create_dossier(ddi,host)
 
@@ -59,21 +62,24 @@ def create_dossier():
 	else:
 		print "Invalid response, received " + str(result["status"][0]) + ": " + str(result["status"][1])
 
-def get_dossier(dossier_id=None):
+def get_dossier(dossier_id=None,ddi=None,host=None):
 	print "---------------------------------"
 	print "Retrieve Dossier"
 	print "---------------------------------"
 	print
 
-	if dossier_id is None:
+	if dossier_id is None and (ddi is None and host is None):
 		dossier_id = raw_input("Dossier ID> ").strip(' ')
 
-	status, result = waldo.get_dossier(dossier=dossier_id)
+	if not dossier_id is None:
+		status, result = waldo.get_dossier(dossier=dossier_id)
+	elif not ddi is None and not host is None:
+		status, result = waldo.get_dossier(ddi=ddi,host=host)
 
 	if status == "FOUND":
 		data = result['data']
 		print "Dossier retreived:"
-		print "\tDossier ID: " + dossier_id
+		print "\tDossier ID: " + result['dossier_id']
 		print "\tDiscovery Status: " + data["discovery_status"]
 		if data["discovery_status"] == "SUCCESS":
 			print "\tTime: " + data['time'].isoformat()
@@ -81,10 +87,17 @@ def get_dossier(dossier_id=None):
 			print "\tNetwork Location: " + data['netloc']
 			print "\tResolved IP Address: " + data['resolved_ip_address']
 			print "\tRegion: " + data['topology']['region']
-			print "\tSummary Page: https://" + waldo.waldoserver + "/gui#/" + data['tenant_id'] + "/dossiers/" + data['netloc'] + "/" + dossier_id + "/summary"
+			print "\tSummary Page: https://" + waldo.waldoserver + "/gui#/" + data['tenant_id'] + "/dossiers/" + data['netloc'] + "/" + result['dossier_id'] + "/summary"
 	else:
 		print "Invalid response, received " + str(result["status"][0]) + ": " + str(result["status"][1])
 
+def get_latest_dossier():
+	print "---------------------------------"
+	print "Retrieve Latest Dossier"
+	print "---------------------------------"
+	print
+
+	get_dossier(ddi=get_ddi(),host=get_host())
 
 def list_handler(ddi=None,host=None):
 	offset = 0
@@ -157,41 +170,55 @@ def list_handler(ddi=None,host=None):
 						print "Invalid Selection."
 
 		else:
-			print "Invalid response, received " + str(result["status"][0]) + ": " + str(result["status"][1])
+			print "Invalid response, received " + str(results["status"][0]) + ": " + str(results["status"][1])
 			return
 
 def find_dossier_by_DDI():
-	print "Currently not implemented."
-
 	print "---------------------------------"
 	print "Get Dossier List By DDI"
 	print "---------------------------------"
 	print
 
-	valid_input = False
-
-	while not valid_input:
-		ddi = raw_input("Please enter DDI: ")
-
-		ddi = ddi.strip(' ')
-
-		if len(ddi) > 0 and ddi.isdigit():
-			ddi = int(ddi)
-			valid_input = True
-		elif len(ddi) == 0:
-			print "No DDI entered, cancelling."
-			return
-		else:
-			print "Invalid DDI Input."
-
-	list_handler(ddi=ddi)
+	list_handler(ddi=get_ddi())
 
 
 def find_dossier_by_DDI_HOST():
-	print "Currently not implemented."
+	print "---------------------------------"
+	print "Get Dossier List By DDI"
+	print "---------------------------------"
+	print
+
+	list_handler(ddi=get_ddi(),host=get_host())
+
+def get_all_dossiers():
+	print "---------------------------------"
+	print "Get Dossier All Dossiers"
+	print "---------------------------------"
+	print
+	list_handler()
 
 def advanced_menu():
-	print "Currently not implemented."
+	while True:
+		print "---------------------------------"
+		print "Get Dossier List By DDI"
+		print "---------------------------------"
+		print
+		print "Choose an option:"
+		print "\t1) Get All Dossiers"
+		print
+		print "\tx) Exit"
+
+		choice = raw_input("> ")
+
+		if len(choice) > 0:
+			choice = choice[0].lower()
+
+			if choice == '1':
+				get_all_dossiers()
+			elif choice == 'x':
+				return
+			else:
+				print "\nInvalid Input!\n"
 
 keep_running = True
 
@@ -204,9 +231,10 @@ while keep_running:
 	print "Choose an option:"
 	print "\t1) Create Dossier"
 	print "\t2) Get Dossier"
-	print "\t3) Find Dossier by DDI"
-	print "\t4) Find Dossier by DDI and Host"
-	print "\t5) Advanced Options"
+	print "\t3) Get Latest Dossier"
+	print "\t4) Find Dossier by DDI"
+	print "\t5) Find Dossier by DDI and Host"
+	print "\t6) Advanced Options"
 	print
 	print "\tq) Quit"
 
@@ -220,10 +248,12 @@ while keep_running:
 		elif choice == '2':
 			get_dossier()
 		elif choice == '3':
-			find_dossier_by_DDI()
+			get_latest_dossier()
 		elif choice == '4':
-			find_dossier_by_DDI_HOST()
+			find_dossier_by_DDI()
 		elif choice == '5':
+			find_dossier_by_DDI_HOST()
+		elif choice == '6':
 			advanced_menu()
 		elif choice == 'q':
 			keep_running = False
